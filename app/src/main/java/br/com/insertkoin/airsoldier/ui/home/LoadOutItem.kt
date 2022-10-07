9d9c3351439705944ddcf7366382de2104e691be
Daniel Tief <chinorex@gmail.com>
2022-10-06 21:43:49 -0300

    Navigation Drawer Layout finished

--- app/src/main/java/br/com/insertkoin/airsoldier/ui/home/LoadOutItem.kt
+++ app/src/main/java/br/com/insertkoin/airsoldier/ui/home/LoadOutItem.kt
@@ -3,5 +3,6 @@ package br.com.insertkoin.airsoldier.ui.home
 data class LoadOutItem(
     val id: Int,
     val title: String,
-    val level: Int
+    val level: Int,
+    val percentage: Float
 )
---
+++ app/src/main/java/br/com/insertkoin/airsoldier/ui/user/User.kt
@@ -0,0 +1,106 @@
+package br.com.insertkoin.airsoldier.ui.user
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import br.com.insertkoin.airsoldier.R
+
+@Composable
+fun UserDetail(
+    modifier: Modifier = Modifier,
+    name: String,
+    tag: String
+) {
+    Row(modifier = modifier.padding(all = 8.dp), verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(R.drawable.ic_airsoldier),
+            contentDescription = stringResource(R.string.app_name),
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+        Spacer(modifier = modifier.width(16.dp))
+        Column {
+            Text(text = tag, style = MaterialTheme.typography.h6)
+            Spacer(modifier = modifier.height(4.dp))
+            Text(text = name, style = MaterialTheme.typography.h4)
+        }
+    }
+}
+
+@Composable
+fun CircularLevelBar(
+    percentage: Float,
+    level: Int,
+    fontSize: TextUnit,
+    radius: Dp,
+    color: Color = Color.LightGray,
+    strokeWidth: Dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+    Box(
+        modifier = Modifier.size(radius * 2f),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier.size(radius * 2f)
+        ) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = level.toString(),
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+fun UserDetailPreview() {
+    Surface {
+        UserDetail(name = "Tief", tag = "Soldado")
+    }
+}
